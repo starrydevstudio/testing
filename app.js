88b9aa0ec543aa46969116b77dc523c469534a52
StarrySkadi <starryskadi@gmail.com>
2020-06-25 21:45:05 +0630

    Add Route Setup

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/",function(req, res) {
     res.render("index", {})
 })
 
+app.get("/setup" ,function(req,res) {
+    res.send("Setup is successful")
+})
+
 app.listen(3000, function() {
     console.log("Server Starting");
 })
